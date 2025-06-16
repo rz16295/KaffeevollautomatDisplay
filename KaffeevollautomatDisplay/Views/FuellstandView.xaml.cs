5f164847671ad542ecc29a65ae5110627023b972
rzierhut <r.zierhut@tgalpha.de>
2025-06-16 18:23:48 +0200

    Füllstand angepasst

--- KaffeevollautomatDisplay/Views/FuellstandView.xaml.cs
+++ KaffeevollautomatDisplay/Views/FuellstandView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace KaffeevollautomatDisplay.Views
 {
@@ -16,8 +18,25 @@ namespace KaffeevollautomatDisplay.Views
 
         private void AktualisiereAnzeige()
         {
+            // Wasser
+            WasserProgress.Maximum = Fuellstand.MaxWasser;
+            WasserProgress.Value = Fuellstand.AktuellerWasser;
             WasserstandText.Text = $"{Fuellstand.AktuellerWasser} ml / {Fuellstand.MaxWasser} ml";
+
+            double wasserProzent = (double)Fuellstand.AktuellerWasser / Fuellstand.MaxWasser;
+            WasserProgress.Foreground = wasserProzent < 0.2
+                ? new SolidColorBrush(Colors.Red)
+                : new SolidColorBrush(Colors.Green);
+
+            // Bohnen
+            BohnenProgress.Maximum = Fuellstand.MaxBohnen;
+            BohnenProgress.Value = Fuellstand.AktuelleBohnen;
             BohnenstandText.Text = $"{Fuellstand.AktuelleBohnen} g / {Fuellstand.MaxBohnen} g";
+
+            double bohnenProzent = (double)Fuellstand.AktuelleBohnen / Fuellstand.MaxBohnen;
+            BohnenProgress.Foreground = bohnenProzent < 0.2
+                ? new SolidColorBrush(Colors.Red)
+                : new SolidColorBrush(Colors.Green);
         }
 
         private void WasserAuffuellen_Click(object sender, RoutedEventArgs e)
@@ -34,12 +53,12 @@ namespace KaffeevollautomatDisplay.Views
 
         private void ZurueckButton_Click(object sender, RoutedEventArgs e)
         {
-            
             ZurueckClicked?.Invoke(this, EventArgs.Empty);
         }
 
         private void SetzeTexte()
         {
+            // Sprachumschaltung der Buttons oben
             if (this.Content is Grid grid && grid.Children[0] is StackPanel buttonPanel)
             {
                 if (buttonPanel.Children[0] is Button wasserBtn)
@@ -52,6 +71,7 @@ namespace KaffeevollautomatDisplay.Views
                     zurueckBtn.Content = SpracheManager.Text("Zurück");
             }
 
+            // Sprachumschaltung der Titel
             if (this.Content is Grid mainGrid && mainGrid.Children[1] is Grid contentGrid)
             {
                 if (contentGrid.Children[0] is StackPanel wasserPanel && wasserPanel.Children[0] is TextBlock wasserTitel)
